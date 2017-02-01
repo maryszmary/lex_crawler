9e011c2a7a3337d1a10069c5883bbb06f5999c42
maryszmary <masha.shejanova@gmail.com>
2017-02-01 21:00:41 +0300

    started writing the crowler

---
+++ crowler.py
@@ -0,0 +1,23 @@
+'''
+this is a crowler for http://onlineslovari.com/slovar_drevnerusskogo_yazyika_vv
+'''
+
+
+from urllib import request
+from urllib import parse
+from lxml import etree
+
+
+ROOT = 'http://onlineslovari.com/slovar_drevnerusskogo_yazyika_vv'
+
+
+def root_walker():
+    pass
+
+
+def main():
+    root_walker()
+
+
+if __name__ == '__main__':
+    main()
